c83a355115a9ca514c1ac30b1f7a7409a9b99f1f
Gisele-123 <giselemigisha53@gmail.com>
2025-01-28 12:08:33 -0800

    Day 2: Implemented View available Trains function

--- railway_reservation_system.c
+++ railway_reservation_system.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
+#include<string.h>
+
+struct Train{
+	int trainNumber;
+	char destination[50];
+	int availableSeats;
+	float ticketPrice;
+};
+
+void viewAvailableTrains( struct Train trains[], int availableTrains){
+	printf("\nAvailable Trains: \n\n");
+	for (int i=0; i<availableTrains; i++){
+		printf("Train No: %d, Destination: %s, Available Seats: %d, Price: %.f RWF\n", trains[i].trainNumber, trains[i].destination, trains[i].availableSeats, trains[i].ticketPrice);
+	}
+}
+
 
 int main(){
+	//day 2
+	
+	struct Train trains[5]={
+		{101, "Kigali", 30, 5000},
+		{102, "Musanze", 25, 4500},
+		{103, "Rubavu", 20, 4000},
+		{104, "Nyondo", 15, 3500},
+		{105, "Huye", 10, 3000}
+	};
+	// ady 1
+	
 	printf("Choose an option: \n\n");
 	printf("1. View available trains\n");
 	printf("2. Book tickets\n");
@@ -13,7 +40,7 @@ int main(){
 	
 	switch (choice){
 		case 1:
-			printf("View available trains feature coming soon!");
+			viewAvailableTrains(trains, 5);
 			break;
 		case 2:
 			printf("Book tickets feature coming soon!");
@@ -27,4 +54,7 @@ int main(){
 		default:
 			printf("\nInvalid user choice, Exit!");
 	}
+	
+	return 0;
+
 }
